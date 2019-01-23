16c1b0e4bf9d2848780fc4172cbb54238e2f9ca5
Shane Chintatkun <schintatkun@gmail.com>
2019-01-22 19:44:37 -0800

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
