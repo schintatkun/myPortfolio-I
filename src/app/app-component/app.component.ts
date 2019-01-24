4c941986698db094c12be2a1ce2c670c83e54297
Shane Chintatkun <schintatkun@gmail.com>
2019-01-23 18:04:31 -0800

    complated add component

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { without } from 'lodash';
 
-library.add(faTimes);
+library.add(faTimes, faPlus);
 
 @Component({
   selector: 'app-root',
@@ -16,6 +16,10 @@ export class AppComponent implements OnInit {
 
   deleteApt(theApt: object){
     this.theList = without(this.theList, theApt);
+  }
+  addApt(theApt: object){
+    this.theList.unshift(theApt);
+    //unshift is JS function pushing at the begining of array
   }
 
   constructor (private http: HttpClient) {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app-component/app.component';
 import { AddComponent } from './components/add/add.component';
@@ -18,7 +19,8 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
